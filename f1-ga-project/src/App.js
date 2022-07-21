752fbfc65225697ed0d6c8b6293abce2519860f9
msalexford <alefor1@gmail.com>
2022-07-21 11:07:53 -0500

    Accessed data, yay! + new component.

--- f1-ga-project/src/App.js
+++ f1-ga-project/src/App.js
@@ -7,23 +7,28 @@ import axios from 'axios'
 // import global variables
 import { BASE_URL } from './globals'
 
-// set up useEffect to support async operations
-
 function App() {
+  const [standings, setStandings] = useState([])
+  const [selectedStandings, setSelectedStandings] = useState(null)
+
+  // set up useEffect to support async operations
   // useEffect takes in two arguments: (1) an anonymous function, followed by (2) a dependency array
   // whatever happens inside of this function will happen right when the component loads
   useEffect(() => {
     // we can put an async function inside of useEffect
     const getData = async () => {
       // make axios call
-      // const response = await axios.get(
-      //   `${BASE_URL}?method=independentExpend&apikey=${process.env.REACT_APP_API_KEY}`
-      // )
-      const response = await axios.get(
-        // `${BASE_URL}/2022.json?callback=myParser`
-        `http://ergast.com/api/f1/2008/driverStandings.json?callback=myParser`
+      const response = await axios.get(`${BASE_URL}/2022/driverStandings.json`)
+      // create setStandings to store standings in state
+      setStandings(
+        response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings
       )
-      console.log(response.data)
+
+      // data we want is:
+      // (1) driver first name --> [number] > Driver > givenName
+      // (2) driver second name --> [number] > Driver > familyName
+      // (2) constructor --> [number] > Constructors > 0 > name
+      // (3) points
     }
     // call getData function (we want our function to fire as soon as the component loads)
     getData()
