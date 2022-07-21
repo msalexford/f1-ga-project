a938cba87f6a1cfca183551d3cdd29d45f5a6592
msalexford <alefor1@gmail.com>
2022-07-21 11:19:48 -0500

    Updates to component StandingList.

---
+++ f1-ga-project/src/components/StandingList.jsx
@@ -0,0 +1,17 @@
+// create a component to display our standings
+
+// create a function
+// pass props - we will eventually need this
+// we need to send standings data to this component by passing props
+const StandingList = (props) => {
+
+
+    // every React functional component has a return statement
+    return (
+        // every return statement contains some sort of parent element
+        <div></div>
+    )
+}
+
+// every functional component has an export
+export default StandingList
