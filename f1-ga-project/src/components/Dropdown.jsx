195a9e371dbd3f1a54d3d7efabf3bd2b40c900e2
msalexford <alefor1@gmail.com>
2023-01-18 14:19:52 -0600

    Clarifying comments.

--- f1-ga-project/src/components/Dropdown.jsx
+++ f1-ga-project/src/components/Dropdown.jsx
@@ -1,26 +1,38 @@
-// import useState
+// Import useState hook from React.
 import { useState } from 'react'
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-// create a dropdown menu for user to select year
+// Create a functional component for user to select year. A functional component is a JS function that accepts an input of properties, and returns HTML that describes the UI.
+// You can only use hooks inside of function components (not class components - classes have their own way to do the same thing that hooks do).
 
 function Dropdown ({selected,setSelected, getData }) {
+    // Destructure inline, because useState always returns an array with two values.
+    // First value is the current state (isActive). Second value is a function that will allow us to update our state (setIsActive).
+    // We can also set a default value. Here, that is useState(false) because at start, the dropdown is closed.
     const [isActive, setIsActive] = useState(false)
+
+    // Create a variable for options to be included in the dropdown.
     const options = [
         '2022','2021','2020','2019','2018','2017','2016','2015'
     ]
-
+    // Return a dropdown menu.
     return (
         <div>
         <div className="dropdown">
+            {/* On click of the dropdown button, set our dropdown's state to open. */}
+            {/* Clicking on the button will toggle its value open and closed. */}
             <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
-               {selected}
-               <span className="fas fa-caret-down"></span>
+            {selected}
+            {/* Down caret on dropdown button. */}
+            <span className="fas fa-caret-down"></span>
             </div>
             {isActive && (
-                <div className="dropdown-content"
-                >
+                // Pass props into dropdown elements.
+                <div className="dropdown-content">
+                    {/* Testing code notes: */}
                     {/* error here, need a unique key when using .map */}
+                    {/* Set states for style change on hover. */}
+                    {/* Map options as dropdown items. */}
                     {options.map((option) => (
                         <div
                             key={option.position}
@@ -31,6 +43,7 @@ function Dropdown ({selected,setSelected, getData }) {
                             className="dropdown-item"
                         >
                         {option}
+                        {/* Testing code notes: */}
                         {/* selected = user year choice? */}
                         {/* {console.log(selected)} */}
                 </div>
@@ -47,4 +60,5 @@ function Dropdown ({selected,setSelected, getData }) {
     )
 }
 
+// Export Dropdown component.
 export default Dropdown
